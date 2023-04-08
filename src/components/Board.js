0c312008441a65bea5c3888951936dd566735c3a
Guang-Lin Wei <guanglinwei@gmail.com>
2023-04-07 22:17:50 -0400

    init deck

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,38 +1,37 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import UICard from './UICard';
 
 function Board() {
-    const deck = useRef([]);
+    const [deck, setDeck] = useState([]);
 
+    // Initialize the deck
     useEffect(() => {
+        const _d = [];
         for (const rank of ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']) {
             for (const suit of ['S', 'H', 'C', 'D']) {
                 // deck.push({
                 //     rank: rank,
                 //     suit: suit
                 // });
-                deck.current = [...deck.current, {
+                _d.push({
                     rank: rank,
                     suit: suit
-                }];
+                });
             }
         }
-
-        console.log(deck);
+        setDeck(_d);
     }, []);
 
     return (
         <div>
             <h1>Poker Flip App</h1>
             <UICard rank={'A'} suit={'H'} />
+            {deck.map((d, i) =>
+                <div key={i}>
+                    {d.rank} {d.suit}
+                </div>)}
         </div>
     );
-}
-
-
-function Deal () {
-
-    return
 }
 
 export default Board;
